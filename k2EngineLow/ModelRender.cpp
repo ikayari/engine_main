2563f5d61984169a5ed2036fc6c10508ba6ae9dd
ikayari <owaatesimaatano@icloud.com>
2022-02-07 12:28:02 +0900

    2月7日　first commit

---
+++ k2EngineLow/ModelRender.cpp
@@ -0,0 +1,93 @@
+﻿#include "k2EngineLowPreCompile.h"
+#include "ModelRender.h"
+
+namespace nsK2EngineLow {
+
+
+	ModelRender::ModelRender()
+	{
+		
+
+	}
+
+	ModelRender::~ModelRender()
+	{
+
+	}
+
+	void ModelRender::Update()
+	{
+		
+		//アニメーションを進める。
+		m_animation.Progress(g_gameTime->GetFrameDeltaTime());
+		if (m_animationClip != nullptr)
+		{
+			m_skeleton.Update(m_model.GetWorldMatrix());
+		}
+		m_model.UpdateWorldMatrix(m_position, m_rotation, m_scale);
+	}
+	void ModelRender::Draw(RenderContext& rc)
+	{
+		m_model.Draw(rc);
+	}
+	void ModelRender::Init(const char* filePath,
+		AnimationClip* animationClips,
+		int numAnimationClips,
+		EnModelUpAxis enModelUpAxis)
+	{
+		ModelInitData initData;
+		//シェーダーファイルのファイルパス。
+		initData.m_fxFilePath = "Assets/shader/model.fx";
+
+
+		//ディレクションライトの情報を定数バッファとしてディスクリプタヒープに登録するために
+		//モデルの初期化情報として渡す。
+		initData.m_expandConstantBuffer = &g_sceneLight.GetLight();
+		initData.m_expandConstantBufferSize = sizeof(g_sceneLight.GetLight());
+		if (animationClips == nullptr)
+		{
+			//ノンスキンメッシュ用の頂点シェーダーのエントリーポイントを指定する。
+			initData.m_vsEntryPointFunc = "VSMain";
+		}
+		else
+		{
+			//スキンメッシュ用の頂点シェーダーのエントリーポイントを指定。
+			initData.m_vsSkinEntryPointFunc = "VSSkinMain";
+			InitSkeleton(filePath);
+			//スケルトンを指定する。
+			initData.m_skeleton = &m_skeleton;
+			InitAnimation(animationClips, numAnimationClips, enModelUpAxis);
+		}
+		initData.m_tkmFilePath = filePath;
+		
+
+
+		m_enFbxUpAxis = enModelUpAxis;
+		initData.m_modelUpAxis = m_enFbxUpAxis;
+		m_model.Init(initData);
+
+	}
+	void ModelRender::InitSkeleton(const char* filePath)
+	{
+		//スケルトンのデータを読み込み。
+		std::string skeletonFilePath = filePath;
+		int pos = (int)skeletonFilePath.find(".tkm");
+		skeletonFilePath.replace(pos, 4, ".tks");
+		m_skeleton.Init(skeletonFilePath.c_str());
+	}
+	void ModelRender::InitAnimation(
+		AnimationClip* animationClip,
+		int numAnimationClips,
+		EnModelUpAxis enModelUpAxis)
+	{
+		m_animationClip = animationClip;
+		m_numAnimationClips = numAnimationClips;
+		m_animation.Init
+		(	m_skeleton,
+			m_animationClip,
+			m_numAnimationClips
+		);
+
+	}
+
+}
---
+++ k2EngineLow/SceneLight.cpp
@@ -0,0 +1,43 @@
+#include "k2EngineLowPreCompile.h"
+#include "SceneLight.h"
+
+
+namespace nsK2EngineLow {
+	SceneLight g_sceneLight;
+
+	SceneLight::SceneLight()
+	{
+		Vector3 dir = { 1.0f,-1.0f,-1.0f };
+		m_light.directionLight.SetDirection({dir});
+
+		m_light.directionLight.SetColor({ 1.0f,1.0f,1.0f });
+
+		SetAmbientLight({0.3f,0.3f,0.3f});
+
+		m_light.pointLight.SetPosition({ 0.0f,50.0f,50.0 });
+
+		m_light.pointLight.SetColor({ 15.0f,0.0f,0.0f });
+
+		m_light.pointLight.SetRange(100.0f);
+
+
+		m_light.spotLight.SetPosition({ 0.0f,50.0f,100.0f });
+
+		m_light.spotLight.SetColor({ 50.0f,50.0f,50.0f });
+		dir = { 1.0f,-1.0f,1.0f };
+		m_light.spotLight.SetDirection(dir);
+
+		m_light.spotLight.SetRange(300.0f);
+
+		m_light.spotLight.SetAngle(Math::DegToRad(25.0f));
+	}
+
+
+
+
+
+
+
+
+
+};
